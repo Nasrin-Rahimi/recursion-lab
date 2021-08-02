f8f380b4fc3edd2c32bbf9630a9b0f07cb37a398
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-02 09:27:01 +0430

    print string charachter by recursive

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// Code your solution here!
+function stringCharacters(string) {
+    console.log(string[0])
+    if(string.length > 1) {
+        stringCharacters(string.substring(1, string.length))
+    }
+    else { 
+        return true
+    }
+}
+
+stringCharacters('nasrin')
+
 
