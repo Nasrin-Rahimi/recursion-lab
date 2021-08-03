a0b89746e4a8002d4a3adcd0eb8ab09f699d330a
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-03 08:59:07 +0430

    recursive function for checking palindrome word

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-function stringCharacters(string) {
+function printString(string) {
     console.log(string[0])
     if(string.length > 1) {
-        stringCharacters(string.substring(1, string.length))
+        printString(string.substring(1, string.length))
     }
     else { 
         return true
     }
 }
 
-stringCharacters('nasrin')
+function reverseString(string) {
+    if(string.length > 1) {
+        return string[string.length - 1] + reverseString(string.substring(0, string.length - 1))
+    }
+    else
+    {
+        return string
+    }
+}
 
+function isPalindrome(word) {
+   if (word.length == 1){
+       return true
+   }
+   else if (word.length == 2){
+        return false
+   } else {
+        if(word[0] === word[word.length - 1]) {
+            let newWord = word.substring(1,word.length - 1)
+            return isPalindrome(newWord)
+        } else {
+            return false
+        }
+   }
+}
+
+// printString('nasrin')
+
+// console.log(reverseString('nasrin'))
+
+console.log(isPalindrome('ababa'))
 
